3d76f6b6d114eb07f9cf7e3ce158ba763625f2a7
lxchub-inst <loic.agst@eduge.ch>
2024-03-05 11:07:16 +0100

    Ajout des commentaires pour des explications plus claires

--- BankingSystem/src/Client.java
+++ BankingSystem/src/Client.java
@@ -7,6 +7,8 @@ public class Client {
     private String prenom;
     private int ID;
     private LocalDate birthdate;
+
+    // On creée une liste de compte pour les stocker
     private ArrayList<Account> accounts;
 
     // Builder d'instance
@@ -15,13 +17,17 @@ public class Client {
         this.prenom = prenom;
         this.ID = ID;
         this.birthdate = birthDate;
+
+        // On initialise la liste dans les éléments du constructeur (et non pas en paramètre)
         this.accounts = new ArrayList<Account>();
     }
 
     public ArrayList<Account> getAccounts(){
+
         return accounts;
     }
 
+    // On crée une méthode qui permet de récupérer pour chaque compte existant
     public Account getAccount(String numCompte){
         for (int i = 0; i < accounts.size(); i++) {
             Account account = accounts.get(i);
@@ -32,12 +38,14 @@ public class Client {
         return null;
     }
 
+    // On crée une méthode qui permet d'ajouter le compte a partir de la classe Account
     public void addAccount(Account account){
         accounts.add(account);
     }
 
     public String toString(){
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        return "Client<Nom=" + nom + ", Prénom=" + prenom + ", ID=" + ID + ", Date de naissance=" + formatter.format(birthdate) + ", Comptes=" + accounts + ">";
+        return "Client<Nom=" + nom + ", Prénom=" + prenom + ", ID=" + ID + ", Date de naissance="
+                + formatter.format(birthdate) + ", Comptes=" + accounts + ">";
     }
 }
--- BankingSystem/src/ClientTest.java
+++ BankingSystem/src/ClientTest.java
@@ -22,12 +22,23 @@ public class ClientTest {
         System.out.println("Comptes: " + client.getAccounts());
     }
 
+    /*
+    * On crée une méthode qui permet de créer des comptes Client
+    * On crée une nouvelle instance de Client avec les informations nécessaires
+    * On scan le fichier grâce à la classe Scanner
+    * On fait une boucle sur chaque ligne -> sc.ligne
+    * On scanne une deuxième fois pour pour les éléments de chaque ligne
+    * On crée une instance de Account pour ajouter les comptes grâce à AccountNumber
+    * On ajoute les comptes avec la variable "client" et on le fait avec la méthode "addAccount"
+    *
+    * */
     private static Client creerClientAvecComptes() {
         Client client = new Client(101, "Daniel", "Smith", LocalDate.of(1980, 6, 5));
         try {
-            Scanner sc = new Scanner(new File("ClientAccountData.txt"));
-            while (sc.hasNextLine()) {
-                String accountNumber = sc.nextLine();
+            Scanner sc_ligne = new Scanner(new File("ClientAccountData.txt"));
+            while (sc_ligne.hasNextLine()) {
+                Scanner sc_element = new Scanner(sc_ligne.nextLine());
+                String accountNumber = sc_element.next();
                 Account account = new Account(accountNumber);
                 client.addAccount(account);
             }
@@ -44,6 +55,13 @@ public class ClientTest {
         System.out.println(client.getAccounts());
     }
 
+    /*
+    * On crée une méthode qui permet d'exécuter des opérations sur des comptes
+    * On crée une condition sur le type d'opération et suivante le cas on débite ou crédite
+    * Si le compte est inexistant, alors on affiche un message, et la boucle s'arrête, à l'inverse
+    * on continue
+    *
+    * */
     private static  void executeFichierDesOperations(Client client, String file){
         try {
             Scanner sc = new Scanner(new File(file));
