3ff0dd6a6463dd04ba425fbf7e58ad9e7849335a
MrsWmps <loic.agst@eduge.ch>
2024-03-06 09:06:11 +0100

    Ajout des commentaires et indications

--- Planetes/src/Informations.java
+++ Planetes/src/Informations.java
@@ -10,14 +10,24 @@ public class Informations {
         // lireDonnes("planetes.txt").forEach(System.out::println);
         ArrayList<Planetes> listePlanetes = lireDonnes("systemeSolaire.txt");
 
+        System.out.println("Liste des planètes : ");
         for (Planetes planete : listePlanetes) {
             System.out.println(planete);
         }
-
+        System.out.println("\n");
+        System.out.println("1e : Calculer la circonférence maximum des planètes ");
         calculerMaxCirconference(listePlanetes);
+        System.out.println("\n");
+        System.out.println("2e : Calculer la circonférence minimum des planètes ");
         calculerMinCirconference(listePlanetes);
+        System.out.println("\n");
+        System.out.println("3e : Calculer le nombre total de satellites dans le système solaire ");
         calculerTotalSatellite(listePlanetes);
+        System.out.println("\n");
+        System.out.println("4e : Calculer le rayon moyen des planètes ");
         calculerRayonMoyenPlanetes(listePlanetes);
+        System.out.println("\n");
+        System.out.println("5e : Calculer les planètes qui n'ont aucun satellite ");
         System.out.println(calculerPlaneteAucunSatellite(listePlanetes));
     }
 
