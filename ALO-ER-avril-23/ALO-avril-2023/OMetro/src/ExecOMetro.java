bc8a5da5db7ab5a6b64de3c20b3845efbb12a981
LOIC.AGST <loic.agst@eduge.ch>
2024-03-10 23:14:09 +0100

    Update ExecOMetro.java

--- ALO-ER-avril-23/ALO-avril-2023/OMetro/src/ExecOMetro.java
+++ ALO-ER-avril-23/ALO-avril-2023/OMetro/src/ExecOMetro.java
@@ -12,7 +12,8 @@ public class ExecOMetro
     // ici on a choisi la correspondance directe : le nombre de zones est la position dans la liste (d'où le -1 en position 0)
     public static final ArrayList<Double>  TARIF_PAR_NB_ZONES /*_traversées*/ = new ArrayList<>(java.util.List.of( -1.0, 2.5, 3.5, 5.0, 5.5));
 
-    public static final String NOM_FICHIER_ARRETS = "C:\\Users\\itslx\\Documents\\GitHub\\ALO\\ALO-ER-avril-23\\ALO-avril-2023\\OMetro\\ligneMM.csv";
+    //public static final String NOM_FICHIER_ARRETS = "C:\\Users\\itslx\\Documents\\GitHub\\ALO\\ALO-ER-avril-23\\ALO-avril-2023\\OMetro\\ligneMM.csv";
+    public static final String NOM_FICHIER_ARRETS = "D:\\ALO\\ALO-ER-avril-23\\ALO-avril-2023\\OMetro\\ligneMM.csv";
     public static final int CODE_ARRET_INCONNU = -1;
 
     //public static final ArrayList<Arret> LISTE_SECOURS_ARRETS = affecterListeSecoursArrets();
@@ -64,7 +65,12 @@ public class ExecOMetro
         int positionSud = 0;
         int positionNord = 0;
 
+
+        // On initialise un compteur pour parcourir la liste des arrêts
+        // Le compteur servira pour la position de l'arrêt
         int compteur = 0;
+
+        // On crée une boucle pour parcourir la liste des arrêts
         for (Arret arret :  ligneMM) {
             if (arret.getDureeVersSud() > maxSud) {
                 maxSud = arret.getDureeVersSud();
@@ -108,7 +114,7 @@ public class ExecOMetro
     * */
     private static int getPositionArret(String nomArret, ArrayList<Arret> ligneMM) {
         for (int i = 0; i < ligneMM.size(); i++) {
-            if (ligneMM.get(i).getNomArret().equals(nomArret)) {
+            if (ligneMM.get(i).getNomArret().equals(nomArret)) { // ligneMM.get(i) -> on récupère l'arrêt à partir de la position i
                 return i;
             }
         }
@@ -117,43 +123,29 @@ public class ExecOMetro
 
     private static void testQuestionB3(String nomArretDepart, String nomArretArrivee, ArrayList<Arret> ligneMM) {
         System.out.println("Trajet de "+nomArretDepart+" à "+nomArretArrivee);
+
+        ArrayList<Arret> lstArrets = new ArrayList<>();
         // On récupère la position de l'arrêt de départ et d'arrivée
         int positionDepart = getPositionArret(nomArretDepart, ligneMM);
         int positionArriver = getPositionArret(nomArretArrivee, ligneMM);
 
-        // On vérifie si l'arrêt est inconnu pour éviter les erreurs
-        if (positionDepart == CODE_ARRET_INCONNU || positionArriver == CODE_ARRET_INCONNU) {
-            System.out.println("L'arrêt est inconnu");
-        }
-
-        // On récupère la position minimale et maximale pour obtenir les arrêts entre les deux positions
-        int minPosition, maxPosition;
-
-        /*
-        * Si la position de départ est inférieur à la position d'arrivée
-        * Alors on instantice la position de départ comme la position minimale et la position d'arrivée comme la position maximale
-        * A l'inverse, on instantie la position d'arrivée comme la position minimale et la position de départ comme la position maximale
-        * */
         if (positionDepart < positionArriver) {
-            minPosition = positionDepart;
-            maxPosition = positionArriver;
+            for (int i = positionDepart; i <= positionArriver; i++) {
+                lstArrets.add(ligneMM.get(i));
+            }
         } else {
-            minPosition = positionArriver;
-            maxPosition = positionDepart;
+            for (int i = positionDepart; i >= positionArriver; i--) {
+                lstArrets.add(ligneMM.get(i));
+            }
         }
-        // Création et remplissage de la nouvelle liste avec les arrêts entre les positions de départ et d'arrivée
-        ArrayList<Arret> lstArrets = new ArrayList<>();
-        for (int i = minPosition; i <= maxPosition; i++) {
-            Arret arret = ligneMM.get(i - 1); // Décalage de 1 pour obtenir l'indice correct dans la liste
-            lstArrets.add(arret);
-        }
+
         /*
          * On crée une boucle pour parcourir la liste des arrêts
          * On récupère l'arrêt à partir de la position minimale et maximale
          * On affiche le nom de l'arrêt
          * */
         System.out.println("Liste des " + lstArrets.size() + " arrêts entre " + nomArretDepart + " à " + nomArretArrivee);
-        for (int i = minPosition; i <= maxPosition; i++) {
+        for (int i = positionDepart; i >= positionArriver; i++) {
             Arret arret = ligneMM.get(i); // Décalage de 1 pour obtenir l'indice correct dans la liste
             System.out.println(arret.getNomArret());
         }
@@ -168,9 +160,10 @@ public class ExecOMetro
         // On compare, comme au B3, la position du départ et de l'arrivée
         if (positionDepart < positionArriver) {
             for (int i = positionDepart; i <= positionArriver; i++) {
-                arretsTrajet.add(ligneMM.get(i));
+                arretsTrajet.add(ligneMM.get(i)); // arretsTrajet.add(ligneMM.get(i)) -> on ajoute l'arrêt à la liste
             }
         } else {
+            // for (int i = positionDepart; i >= positionArriver; i--) -> on parcourt la liste à l'envers pour obtenir les arrêts entre les deux positions
             for (int i = positionDepart; i >= positionArriver; i--) {
                 arretsTrajet.add(ligneMM.get(i));
             }
@@ -183,13 +176,12 @@ public class ExecOMetro
             System.out.println(arret.getNomArret());
         }
         System.out.println("\n");
-        
+
         // On crée une variable pour stocker la zone, en appelant de la liste "arretTrajet"
-        String zoneDepart = arretsTrajet.get(0).getNomZone();
+        String zoneDepart = arretsTrajet.get(0).getNomZone(); // arretsTrajet.get(0) -> on récupère le premier arrêt de la liste, soit on récupère la zone de départ
         
         // On initialise la variable de la zone à 1
         int nbZonesTraverses = 1;
-        
         /* 
         * A chaque tour de boucle, on récupère le nom des arrêts
         * Si l'arrêt récupéré corresponds bien à la zone définis
@@ -199,14 +191,14 @@ public class ExecOMetro
             Arret arretActuel = arretsTrajet.get(i);
             if (!arretActuel.getNomZone().equals(zoneDepart)) {
                 nbZonesTraverses++;
-                zoneDepart = arretActuel.getNomZone();
+                zoneDepart = arretActuel.getNomZone(); // zoneDepart = arretActuel.getNomZone() -> on stocke la zone de départ à chaque tour de boucle
             }
         }
         
         // On affiche le résultat étant donné que TARIF_NB_PAR_ZONE est une liste, et 
         // bien la variable en paramètre est le nombre de zone
         System.out.println("Tarif appliqué pour " + nbZonesTraverses + " zones traversées: "
-                + TARIF_PAR_NB_ZONES.get(nbZonesTraverses) + " euros");
+                + TARIF_PAR_NB_ZONES.get(nbZonesTraverses) + " euros"); // TARIF_PAR_NB_ZONES.get(nbZonesTraverses) -> on récupère le tarif à partir du nombre de zones traversées
     }
 
     //vos méthodes ici
